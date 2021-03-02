6cc614fb803fb60ce5d31f701d0fc5cd8bbb9caa
Mahadi638 <mdmahadihasan6198@gmail.com>
2021-03-02 19:14:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+//letter grad
+
+var marks = prompt ("Enter your mmarks :  ");
+
+if ( marks >= 80 && marks <= 100 )
+console.log ("A+");
+
+else if ( marks >= 70 && marks <= 100 )
+console.log ("A");
+
+else if ( marks >= 60 && marks <= 89 )
+console.log ("A-");
+
+else if ( marks >= 50 && marks <= 69 )
+console.log ("B");
+
+else if ( marks >= 40 && marks <= 56 )
+console.log ("C");
+
+else if ( marks >= 33 && marks <= 49 )
+console.log ("D");
+
+else 
+console.log ("Fail");
+
+
+
+//  letter grad end  //
+
+
+
+
+for (var x=1;x <= 10; x=x+1) {
+    document.write("<h1>bangladsh<h1>");
+}
+
+
+
+
+for (var x=1;x <= 10; x=x+1) {
+    document.write(""  +  x);
+}
+    document.write("<h1>number<h1>");
+
+
+
+
+    for (var x=1;x <= 99; x=x+2) {
+        document.write(""  +  x);
+    }
+        document.write("<h1>number<h1>");
+
+
+
+        for (var x=10;x >= 1; x=x-1) {
+            document.write(""  +  x);
+        }
+            document.write("<h1>number<h1>");
+
+
+
+           
+
+           
